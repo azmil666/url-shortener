b8a8868ea8df34e88b4bdf3c80dc31d61ad36045
Azmil Mohammed <azmilmohammed369@gmail.com>
2026-04-09 18:14:34 +0530

    deploy error #6

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import cors from "cors";
 import urlRoutes from "./routes/url.js";
 
+dotenv.config();
+
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+  })
+);
 app.use(express.json());
 
 app.use("/", urlRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-async function startServer() {
-  try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI is missing");
-    }
-
-    await mongoose.connect(process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     console.log("Connected to MongoDB");
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
     });
-
-  } catch (err) {
-    console.error("FATAL ERROR:", err.message);
-    process.exit(1);
-  }
-}
-
-startServer();
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
