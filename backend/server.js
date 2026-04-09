f47d95dadc83acf8cc9b7522ee0cc9906d8d331f
Azmil Mohammed <azmilmohammed369@gmail.com>
2026-04-09 17:54:41 +0530

    deploy error #2

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,35 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import cors from "cors";
-import urlRoutes from "./routes/url.js";
-
-dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import urlRoutes from './routes/url.js';
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST"],
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/", urlRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
+const PORT = process.env.PORT || 10000;
+
+
+console.log("ENV CHECK:");
+console.log("MONGO_URI:", process.env.MONGO_URI ? "FOUND" : "MISSING");
+console.log("PORT:", process.env.PORT);
+
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-  });
+
+  } catch (err) {
+    console.error("FATAL ERROR:", err);
+    process.exit(1);
+  }
+}
+
+startServer();
