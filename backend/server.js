e941170956ae81f8ebcfbdbbbdd31e51c97f14df
Azmil Mohammed <azmilmohammed369@gmail.com>
2026-04-09 18:23:46 +0530

    deploy error #7

--- backend/server.js
+++ backend/server.js
@@ -14,18 +14,25 @@ app.use(
     methods: ["GET", "POST"],
   })
 );
-app.use(express.json());
 
+app.use(express.json());
 app.use("/", urlRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+console.log("MONGO_URI:", process.env.MONGO_URI ? "OK" : "MISSING");
+console.log("PORT:", PORT);
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
   });
